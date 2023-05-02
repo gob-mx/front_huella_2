598da28142a491168b8ea1271c3858c6fdc58e28
Arturo Venegas <arturo.venegaso@gmail.com>
2023-05-02 01:22:57 -0600

    Actualizació foreing key en tablas moduls y permissions

--- database/migrations/2022_10_02_045617_create_moduls_permissions_table.php
+++ database/migrations/2022_10_02_045617_create_moduls_permissions_table.php
@@ -13,17 +13,31 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::create('moduls', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->nullable()->unique();
+            $table->string('acronym')->nullable()->unique();
+            $table->boolean('active')->nullable()->default(1);
+            $table->timestamps();
+
+            $table->unique(['name', 'acronym']);
+        });
+
         Schema::create('moduls_permissions', function (Blueprint $table) {
             $table->unsignedBigInteger('permission_id');
             $table->unsignedBigInteger('modul_id');
-        });
 
-        Schema::create('moduls', function (Blueprint $table) {
-            $table->id();
-            $table->string('name')->nullable();
-            $table->string('acronym')->nullable();
-            $table->boolean('active')->nullable()->default(1);
-            $table->timestamps();
+            $table->foreign('permission_id')
+            ->references('id')
+            ->on('permissions')
+            ->onDelete('cascade');
+
+            $table->foreign('modul_id')
+            ->references('id')
+            ->on('moduls')
+            ->onDelete('cascade');
+
+            $table->primary(['permission_id', 'modul_id'], 'moduls_permissions_permission_id_modul_id_primary');
         });
     }
 
@@ -34,7 +48,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('moduls_permissions');
         Schema::dropIfExists('moduls');
+        Schema::dropIfExists('moduls_permissions');
     }
 };
