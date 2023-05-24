97f3973335d30b7c891351bc64882c061d6afc93
Arturo Venegas <arturo.venegaso@gmail.com>
2023-05-23 18:50:36 -0600

    Migraciones domicilio implicado y domicilio delito

---
+++ database/migrations/2023_05_24_004043_create_domicilio_persona_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('domicilio_persona', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('calle')->nullable();
+            $table->string('numero_exterior')->nullable();
+            $table->string('numero_interior')->nullable();
+            $table->string('colonia')->nullable();
+            $table->string('delegacion_municipio')->nullable();
+            $table->unsignedInteger('codigo_postal')->nullable();
+            $table->string('ciudad')->nullable();
+            $table->string('pais')->nullable();
+            $table->unsignedBigInteger('persona_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('domicilio_persona');
+    }
+};
---
+++ database/migrations/2023_05_24_004241_create_domicilio_delito_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('domicilio_delito', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('pais')->nullable();
+            $table->string('calle')->nullable();
+            $table->string('numero_exterior')->nullable();
+            $table->string('numero_interior')->nullable();
+            $table->string('colonia')->nullable();
+            $table->string('delegacion_municipio')->nullable();
+            $table->string('codigo_postal')->nullable();
+            $table->unsignedBigInteger('carpeta_investigacion_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('domicilio_delito');
+    }
+};
